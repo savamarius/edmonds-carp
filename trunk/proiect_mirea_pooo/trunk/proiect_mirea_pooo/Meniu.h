b5cd9ecf04bd70a273c77c3ffb9cf83ac5812cb7
the_onlyy_one@yahoo.com <the_onlyy_one@yahoo.com@db0e4d25-f12c-51fb-e47e-51a9457a97c3>
2014-08-06 12:23:23 +0000

    meniu facut

---
+++ trunk/proiect_mirea_pooo/trunk/proiect_mirea_pooo/Meniu.h
@@ -0,0 +1,77 @@
+#pragma once
+//#include<iostream>
+
+class Meniu
+{
+private:
+	void start_page()
+	{
+		int optiune = 0;
+
+		std::cout << "    Aplicatie pentru detectarea documentelor plagiat!\n"
+			"			Bine a-ti venit! \n Va rog sa introduceti datele necesare! 		"
+			"	Meniu! \n"
+			"=====================================================\n"
+			"	1) Scanare completa (fara limita inferioara)\n"
+			"	2) Scanare completa (cu limita inferioara)\n"
+			"	--------------------------------------------\n"
+			"	3) Exit"
+			"=====================================================\n"
+			"		Optiunea dumneavoastra este : \n    ";
+		std::cin >> optiune;
+		if (optiune == 1)
+			C_scan_No_Limit();
+		else if (optiune == 2)
+			C_scan_Limit();
+		else if (optiune == 3)
+			exit;
+
+	
+	}
+
+	void C_scan_No_Limit()
+	{
+		
+		char fisier_copie[200], plagiat[200];//dupa fisierul plagiat a fost facut cel copiat.... se va verifica cel copiat
+		system("cls");
+		std::cout << " Trebuie sa introduceti cele doua fisiere:	\n"
+			"Primul fisier este cel considerat copie ( introduceti adresa): ";
+		std::cin >> fisier_copie;
+
+
+		std::cout << "\n  Cel de-al doilea fisier este cel considerat plagiat ( introduceti adresa ) :   ";
+		std::cin >> plagiat;
+
+		//cele 2 fisiere trebuie trimise mai departe catre sistemul principal de scanare
+	
+	}
+
+
+	void C_scan_Limit() // se va considera plagiat doar daca textug comun va fi asemanator  dupa un anumit procentaj
+	{
+		float limit = 0;
+		char fisier_copie[200], plagiat[200];
+		system("cls");
+		std::cout << "Introduceti procentajul de asemanare dorit: ";
+		std::cin >> limit;
+
+		std::cout << " Trebuie sa introduceti cele doua fisiere:	\n"
+			"Primul fisier este cel considerat copie ( introduceti adresa): ";
+		std::cin >> fisier_copie;
+
+
+		std::cout << "\n  Cel de-al doilea fisier este cel considerat plagiat ( introduceti adresa ) :   ";
+		std::cin >> plagiat;
+
+	// se va apela metoda din sisteul central de scanare.....
+	};
+public:
+
+
+	void Menu_Method()
+	{
+		this->start_page();
+	
+	};
+
+};
